a24df877c59e341e97e69151ad2d7c226f7fa2ca
chlonel <chlonel27@gmail.com>
2021-06-06 18:32:27 +0900

    surroundをencloseに

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,40 @@ import CodeMirror from 'codemirror';
 type PositionType = 'anchor' | 'head';
 const positionTypeArray: PositionType[] = ['anchor', 'head'];
 
-const leftSurroundingChar = "'";
-const rightSurroundingChar = "'";
+const leftEnclosure = "'";
+const rightEnclosure = "'";
 
 const keydownCallback = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
   const { key } = event;
-  if (key !== leftSurroundingChar) return;
+  if (key !== leftEnclosure) return;
   if (!cm.somethingSelected()) return;
 
   event.preventDefault();
 
   const beforeReplacementSelections = cm.listSelections();
   const selectionStrings = cm.getSelections();
-  const replacedSelections = selectionStrings.map(
-    selectionString => leftSurroundingChar + selectionString + rightSurroundingChar
-  );
+  const replacedSelections = selectionStrings.map(selectionString => leftEnclosure + selectionString + rightEnclosure);
   cm.replaceSelections(replacedSelections);
 
-  const addedSurroundingCharCounter = {} as Record<number, number>;
-  const insideSurroundingCharSelections = beforeReplacementSelections.map(selection => {
+  const addedEnclosureCounter = {} as Record<number, number>;
+  const insideEnclosureSelections = beforeReplacementSelections.map(selection => {
     const sortedPositionTypeArray = positionTypeArray.sort((a, b) => {
       return selection[a].line - selection[b].line || selection[a].ch - selection[b].ch;
     });
     return sortedPositionTypeArray.reduce((acc, positionType, i) => {
       const { line } = selection[positionType];
-      addedSurroundingCharCounter[line] = (addedSurroundingCharCounter[line] || 0) + 1;
+      addedEnclosureCounter[line] = (addedEnclosureCounter[line] || 0) + 1;
 
       const position = {
         line: selection[positionType].line,
-        ch: selection[positionType].ch + addedSurroundingCharCounter[line],
+        ch: selection[positionType].ch + addedEnclosureCounter[line],
       };
       if (i === 1) --position.ch;
 
       return { ...acc, [positionType]: position };
     }, {} as CodeMirror.Range);
   });
-  cm.setSelections(insideSurroundingCharSelections);
+  cm.setSelections(insideEnclosureSelections);
 };
 
 export const activate = async () => {
