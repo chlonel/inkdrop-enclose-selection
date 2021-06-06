22ab757f4c6a7a69252cc79deee9a99758fb26e9
chlonel <chlonel27@gmail.com>
2021-06-06 16:27:33 +0900

    プラグインを無効にすると処理も停止するように

--- src/index.ts
+++ src/index.ts
@@ -6,44 +6,51 @@ const positionTypeArray: PositionType[] = ['anchor', 'head'];
 const leftSurroundingChar = "'";
 const rightSurroundingChar = "'";
 
+const keydownCallback = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
+  const { key } = event;
+  if (key !== leftSurroundingChar) return;
+  if (!cm.somethingSelected()) return;
+
+  event.preventDefault();
+
+  const beforeReplacementSelections = cm.listSelections();
+  const selectionStrings = cm.getSelections();
+  const replacedSelections = selectionStrings.map(
+    selectionString => leftSurroundingChar + selectionString + rightSurroundingChar
+  );
+  cm.replaceSelections(replacedSelections);
+
+  const addedSurroundingCharCounter = {} as Record<number, number>;
+  const insideSurroundingCharSelections = beforeReplacementSelections.map(selection => {
+    const sortedPositionTypeArray = positionTypeArray.sort((a, b) => {
+      return selection[a].line - selection[b].line || selection[a].ch - selection[b].ch;
+    });
+    return sortedPositionTypeArray.reduce((acc, positionType, i) => {
+      const { line } = selection[positionType];
+      addedSurroundingCharCounter[line] = (addedSurroundingCharCounter[line] || 0) + 1;
+
+      const position = {
+        line: selection[positionType].line,
+        ch: selection[positionType].ch + addedSurroundingCharCounter[line],
+      };
+      if (i === 1) --position.ch;
+
+      return { ...acc, [positionType]: position };
+    }, {} as CodeMirror.Range);
+  });
+  cm.setSelections(insideSurroundingCharSelections);
+};
+
 export const activate = async () => {
   if (!inkdrop.isEditorActive()) {
     await new Promise(resolve => inkdrop.onEditorLoad(resolve));
   }
 
   const editor = inkdrop.getActiveEditor();
-  editor.cm.on('keydown', (cm: CodeMirror.Editor, event: KeyboardEvent) => {
-    const { key } = event;
-    if (key !== leftSurroundingChar) return;
-    if (!cm.somethingSelected()) return;
+  editor.cm.on('keydown', keydownCallback);
+};
 
-    event.preventDefault();
-
-    const beforeReplacementSelections = cm.listSelections();
-    const selectionStrings = cm.getSelections();
-    const replacedSelections = selectionStrings.map(
-      selectionString => leftSurroundingChar + selectionString + rightSurroundingChar
-    );
-    cm.replaceSelections(replacedSelections);
-
-    const addedSurroundingCharCounter = {} as Record<number, number>;
-    const insideSurroundingCharSelections = beforeReplacementSelections.map(selection => {
-      const sortedPositionTypeArray = positionTypeArray.sort((a, b) => {
-        return selection[a].line - selection[b].line || selection[a].ch - selection[b].ch;
-      });
-      return sortedPositionTypeArray.reduce((acc, positionType, i) => {
-        const { line } = selection[positionType];
-        addedSurroundingCharCounter[line] = (addedSurroundingCharCounter[line] || 0) + 1;
-
-        const position = {
-          line: selection[positionType].line,
-          ch: selection[positionType].ch + addedSurroundingCharCounter[line],
-        };
-        if (i === 1) --position.ch;
-
-        return { ...acc, [positionType]: position };
-      }, {} as CodeMirror.Range);
-    });
-    cm.setSelections(insideSurroundingCharSelections);
-  });
+export const deactivate = () => {
+  const editor = inkdrop.getActiveEditor();
+  editor.cm.off('keydown', keydownCallback);
 };
